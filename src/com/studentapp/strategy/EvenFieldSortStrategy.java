bb36e6198e0cb261e7e1642a2c0f81b519ab67c1
Lisa Selezneva <mouse2962@gmail.com>
2025-12-29 11:39:53 +0500

    Переопределён метод sort() в наследнике.

--- src/com/studentapp/strategy/EvenFieldSortStrategy.java
+++ src/com/studentapp/strategy/EvenFieldSortStrategy.java
@@ -6,7 +6,7 @@ public abstract class EvenFieldSortStrategy implements EvenSortStrategy {
     protected abstract int getSortField(Student student);
 
     @Override
-    public final void sort(Student[] students, int size) {
+    public void sort(Student[] students, int size) {
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (isEven(getSortField(students[i])) && isEven(getSortField(students[j]))) {
